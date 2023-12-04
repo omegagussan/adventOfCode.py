df997bd59d2b7150c24b4d5cee848b00a170bc42
Gustav Sandström <gustavs@spotify.com>
2023-12-04 15:31:09 +0100

    without dynamic programing

--- src/days/day4/main.py
+++ src/days/day4/main.py
@@ -14,17 +14,17 @@ def part1():
 
 
 def part2():
-	with open("input.txt") as input_file:
+	with open("sample.txt") as input_file:
 		total = 0
 		rows = input_file.read().strip().split('\n')
-		original = {int(row.split(": ")[0][5::]): row.split(": ")[1].strip() for row in rows}
-		print(original)
+		original = {int(row.split(": ")[0][5::]): tuple(i.split() for i in row.split(": ")[1].strip().split(" | ")) for row in rows}
+		# counts = {k: 1 for k in original.keys()}
 		stack = list(original.items())
 		while stack:
 			nr, row = stack.pop(0)
 			total += 1
-			winners, candidates = row.split(" | ")
-			overlap = set(winners.split()).intersection(candidates.split())
+			winners, candidates = row
+			overlap = set(winners).intersection(candidates)
 			indexes = [int(nr) + index + 1 for index in range(len(overlap))]
 			for index in indexes:
 				stack.append((index, original.get(index)))
