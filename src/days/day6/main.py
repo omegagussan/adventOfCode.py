152ab2ce93201a4004b0de5d2c85a14d344f73ea
Gustav Sandström <gustavs@spotify.com>
2023-12-06 08:04:26 +0100

    part2

--- src/days/day6/main.py
+++ src/days/day6/main.py
@@ -22,5 +22,20 @@ def part1():
 	return reduce((lambda x, y: x * y), ways_to_race)
 
 
+def part2():
+	with open("input.txt") as input_file:
+		rows = input_file.read().strip().split('\n')
+		time = int(''.join(rows[0].split(' ')[1::]))
+		distance = int(''.join(rows[1].split(' ')[1::]))
+		ways_to_win = 0
+		for charge in range(1, time):
+			run_time = (time - charge)
+			run_distance = charge * run_time
+			if run_distance > distance:
+				ways_to_win += 1
+	return ways_to_win
+
+
 if __name__ == "__main__":
 	print(part1())
+	print(part2())
