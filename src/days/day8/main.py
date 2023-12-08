bc5e15c9965bbc5996ca55a5b9ccc3a315b98143
Gustav Sandström <gustavs@spotify.com>
2023-12-08 11:35:54 +0100

    part2

--- src/days/day8/main.py
+++ src/days/day8/main.py
@@ -1,4 +1,32 @@
+import math
+from functools import reduce
+
+
+def lcm(arr):
+	return reduce(lambda x, y: (x * y) // math.gcd(x, y), arr)
+
+
 def part1():
+	with open("input.txt") as input_file:
+		instructions, map_str = input_file.read().strip().split('\n\n')
+		mapping = {row[:3]: row.split(' = (')[-1][:-1].split(', ') for row in
+				   map_str.split('\n')}
+
+	return solve(instructions, mapping, "AAA", "ZZZ")
+
+
+def solve(instructions, mapping, start, end):
+	curr = start
+	counter = 0
+	while not curr.endswith(end):
+		instruction = instructions[counter % len(instructions)]
+		int_instruction = 0 if instruction == "L" else 1
+		curr = mapping[curr][int_instruction]
+		counter += 1
+	return counter
+
+
+def part2():
 	with open("input.txt") as input_file:
 		instructions, map_str = input_file.read().strip().split('\n\n')
 		print(instructions)
@@ -6,19 +34,13 @@ def part1():
 				   map_str.split('\n')}
 		print(mapping)
 
-	curr = "AAA"
-	print(curr)
-	counter = 0
-	while curr != "ZZZ":
-		instruction = instructions[counter % len(instructions)]
-		print(instruction)
-		int_instruction = 0 if instruction == "L" else 1
-		curr = mapping[curr][int_instruction]
-		counter += 1
-		print(curr)
-
-	return counter
+	curr = [key for key in mapping.keys() if key.endswith('A')]
+	print(len(curr))
+	periods = [solve(instructions, mapping, c, "Z") for c in curr]
+	print(periods)
+	return lcm(periods)
 
 
 if __name__ == "__main__":
 	print(part1())
+	print(part2())
