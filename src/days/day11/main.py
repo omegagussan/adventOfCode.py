ef3a125b5107bc1c3fdd6a859ebbe61eeb2dd7d9
gustav sandström <gussan@pop-os.localdomain>
2023-12-11 20:48:34 +0100

    cleanup

--- src/days/day11/main.py
+++ src/days/day11/main.py
@@ -1,60 +1,69 @@
+from itertools import permutations
 from functools import reduce
 
-threshold: dict[str, int] = {
-	"red": 12,
-	"green": 13,
-	"blue": 14
-}
+spacer = "^"
+replacer = 1000000
 
 
-def check(color_claim: str) -> bool:
-	color = color_claim.split(' ')[1]
-	return threshold[color] >= int(color_claim.split(' ')[0])
-
-
-def update_max(color_claim: str, maxes: dict[str, int]) -> None:
-	color = color_claim.split(' ')[1]
-	if maxes[color] < int(color_claim.split(' ')[0]):
-		maxes[color] = int(color_claim.split(' ')[0])
-
-
-def power(maxes: dict[str, int]) -> int:
-	return reduce((lambda x, y: x * y), maxes.values())
-
+def transpose(mat):
+	return [list(i) for i in zip(*mat)]
 
 def part1():
 	with open("input.txt") as input_file:
-		total = 0
-		games = input_file.read().strip().split('\n')
-		for game in games:
-			failed = False
-			claims = ''.join(game.split(': ')[1]).split("; ")
-			for claim in claims:
-				color_claim = claim.split(', ')
-				if any(map(lambda x: not check(x), color_claim)):
-					failed = True
-					break
-			# add row number if success
-			if not failed:
-				total += int(''.join(game.split(': ')[0]).split(' ')[1])
-	return total
+		rows = input_file.read().strip().split('\n')
+		universe = expand(rows)
+		universe_t = transpose(universe)
+		output_t = expand(universe_t)
+		universe = transpose(output_t)
+		galaxies = get_galaxies(universe)
+		pairs = list(permutations(galaxies, 2))
+		dists = [abs(p[0][0] - p[1][0]) + abs(p[0][1] - p[1][1]) for p in pairs]
+	return reduce((lambda x, y: x + y), dists)// 2
+
+
+def get_galaxies(universe):
+	coordinates = []
+	for row_idx, row in enumerate(universe):
+		for col_idx, col in enumerate(row):
+			if col == "#":
+				coordinates.append((row_idx, col_idx))
+	return coordinates
+
+
+def expand(source, replacement="."):
+	universe = []
+	for row in source:
+		r = list(row)
+		if not "#" in row:
+			universe.append([replacement for _ in r])
+			if replacement != ".":
+				continue
+		universe.append(r)
+	return universe
+
 
 
 def part2():
 	with open("input.txt") as input_file:
+		rows = input_file.read().strip().split('\n')
+		universe = expand(rows, spacer)
+		universe_t = transpose(universe)
+		output_t = expand(universe_t, spacer)
+		universe = transpose(output_t)
+		galaxies = get_galaxies(universe)
 		total = 0
-		games = input_file.read().strip().split('\n')
-		for game in games:
-			maxes: dict[str, int] = {"red": 0, "blue": 0, "green": 0}
-			claims = ''.join(game.split(': ')[1]).split("; ")
-			for claim in claims:
-				color_claims = claim.split(', ')
-				for color_claims in color_claims:
-					update_max(color_claims, maxes)
-			total += power(maxes)
+		pairs = list(permutations(galaxies, 2))
+		for p in pairs:
+			for x in range(p[0][0], p[1][0]):
+				total += replacer if universe[x][p[0][1]] == spacer else 1
+			for y in range(p[0][1], p[1][1]):
+				total += replacer if universe[p[1][0]][y] == spacer else 1
+	
 	return total
 
 
 if __name__ == "__main__":
 	print(part1())
 	print(part2())
+	#93647328 (forgot to change to 10⁶ in replacer
+	#857986849428
