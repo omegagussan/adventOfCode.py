025bad95d5774f077b99b2e62a3f9289e528e605
gustav sandström <gussan@pop-os.localdomain>
2023-12-12 00:01:04 +0100

    day 10 part 2

--- src/days/day10/main.py
+++ src/days/day10/main.py
@@ -1,5 +1,4 @@
-import numpy as np
-from itertools import pairwise
+from functools import reduce
 
 
 # | is a vertical pipe connecting north and south.
@@ -34,9 +33,12 @@ def neighbours(pos, board):
 def filter_is_pipe(starts, board):
 	return list(filter(lambda x: get_value(x, board) != ".", starts))
 
+def filter_is_not_pipe(starts, board):
+	return list(filter(lambda x: get_value(x, board) == ".", starts))
+
 
 def filter_in_board(values, board):
-	return list(filter(lambda p: 0 <= p[0] <= len(board) and 0 <= p[1] <= len(board[0]), values))
+	return list(filter(lambda p: 0 <= p[0] < len(board) and 0 <= p[1] < len(board[0]), values))
 
 def add(t1, t2):
 	return t1[0] + t2[0], t1[1] + t2[1]
@@ -52,22 +54,43 @@ def find_start(mat):
 
 
 def part1():
-	with open("input.txt") as input_file:
-		rows = input_file.read().strip().split('\n')
-		board = [list(row) for row in rows]
-		start =  find_start(board)
-	starts = filter_is_pipe(neighbours(start, board), board)
+	board, start = get_board_start()
+	visited = get_loop(board, start)
+	return len(visited) // 2
 
+def part2():
+	board, start = get_board_start()
+	loop = get_loop(board, start)
+	
+	board_poses = [(y, x) for y, line in enumerate(board) for x, c in enumerate(line)]
+	
+	non_pipe_candidates = list(filter(lambda x: str(x) not in loop, board_poses))
+	inside_loop = [1 if count_crosses(board, c, loop) % 2 == 1 else 0 for c in non_pipe_candidates]
+	return reduce(lambda x, y: x+y, inside_loop)
+
+
+def count_crosses(board, c, loop):
+	crosses = 0
+	ray_pose = tuple(c)
+	while filter_in_board([ray_pose], board):
+		ray_value = get_value(ray_pose, board)
+		if str(ray_pose) in loop and ray_value != "L" and ray_value != "7":
+			crosses += 1
+		ray_pose = add(ray_pose, (1, 1))
+	return crosses
+
+
+def get_loop(board, start):
+	starts = filter_is_pipe(neighbours(start, board), board)
 	curr_pose = starts[0]
 	curr_val = get_value(curr_pose, board)
 	visited = [str(curr_pose)]
-	
 	while curr_val != "S":
 		next_poses = filter_in_board([add(curr_pose, c) for c in parse(curr_val)], board)
 		for pose in next_poses:
-			#because lists are not hashable
+			# because lists are not hashable
 			str_pose = str(pose)
-			#very ugly workaround to not just go to the goal. Sorry!
+			# very ugly workaround to not just go to the goal. Sorry!
 			if get_value(pose, board) == "S" and len(visited) == 1:
 				continue
 			elif str_pose not in visited:
@@ -75,9 +98,17 @@ def part1():
 				curr_val = get_value(curr_pose, board)
 				visited.append(str_pose)
 				break
-		
-	return len(visited) // 2
+	return visited
+
+
+def get_board_start():
+	with open("input.txt") as input_file:
+		rows = input_file.read().strip().split('\n')
+		board = [list(row) for row in rows]
+		start = find_start(board)
+	return board, start
 
 
 if __name__ == "__main__":
-	print(part1())
+	#print(part1())
+	print(part2())
