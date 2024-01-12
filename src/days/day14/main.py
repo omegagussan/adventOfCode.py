a1132490be4fa737f0c395089fa411eaacb2abe8
Gustav Sandström <gussan@pop-os.localdomain>
2024-01-12 08:52:26 +0100

    day 14 part 2

--- src/days/day14/main.py
+++ src/days/day14/main.py
@@ -1,14 +1,18 @@
 from functools import reduce
 
 def rotate_matrix(m):
-	return [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
-
+	return list(x[::-1] for x in zip(*m))
 def flatmap(reverse):
 	return [a for b in reverse for a in b]
 
 def transpose(mat):
 	return [list(i) for i in zip(*mat)]
 
+def spin_cycle(mat):
+	for _ in range(4):
+		mat = tilt(mat)
+		mat = rotate_matrix(mat)
+	return mat
 def tilt(mat):
 	mat_t = transpose(mat)
 	for idx in range(1, len(mat)):
@@ -27,28 +31,59 @@ def tilt(mat):
 			mat = transpose(mat_t)
 	return mat
 def part1():
-	total = 0
 	with open("input.txt") as input_file:
 		rows = input_file.read().strip().split('\n')
 		mat = [list(row) for row in rows]
 		mat = tilt(mat)
 		max_leverage = len(mat)
-		for idx, row in enumerate(mat):
-			count = len(list(filter(lambda x: x == "O", row)))
-			total += (max_leverage - idx) * count
+	return count_leverage(mat, max_leverage)
+
+
+def count_leverage(mat, max_leverage):
+	total = 0
+	for idx, row in enumerate(mat):
+		count = len(list(filter(lambda x: x == "O", row)))
+		total += (max_leverage - idx) * count
 	return total
 
+
+def string_hash(matrix):
+	rows = [','.join(m) for m in matrix]
+	return ';'.join(rows)
+
+def pretty_print(matrix):
+	for row in matrix:
+		print(row)
 def part2():
-	total = 0
 	with open("input.txt") as input_file:
+		total_spins = 1000000000
 		rows = input_file.read().strip().split('\n')
 		mat = [list(row) for row in rows]
-		mat = tilt(mat)
-		max_leverage = len(mat)
-		for idx, row in enumerate(mat):
-			count = len(list(filter(lambda x: x == "O", row)))
-			total += (max_leverage - idx) * count
-	return total
+		cache = {}
+		use_cache = True
+		spin = 0
+		while spin < total_spins:
+			if spin % 10000 == 0:
+				print(spin)
+			mat = spin_cycle(mat)
+			#pretty_print(mat)
+			#print(" ")
+			if not use_cache:
+				spin += 1
+				continue
+			id = string_hash(mat)
+			if id in cache:
+				use_cache = False
+				period = spin - cache[id]
+				remainder = (total_spins - spin) % period
+				spin = total_spins - remainder
+				#print(period)
+				#print(remainder)
+				#print(spin)
+				#print(" ")
+			cache[id] = spin
+			spin += 1
+	return count_leverage(mat, len(mat))
 
 def count_rocks(mat):
 	is_rocks = list(map(lambda x: 1 if x == "O" else 0, flatmap(mat)))
@@ -56,5 +91,5 @@ def count_rocks(mat):
 
 
 if __name__ == "__main__":
-	print(part1())
+	#print(part1())
 	print(part2())
