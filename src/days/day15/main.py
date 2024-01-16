9feefb142566f6533326b7dcb1b221ae3efc5ff2
Gustav Sandström <gustav.sandstroem@gmail.com>
2024-01-16 08:55:17 +0100

    day 15

--- src/days/day15/main.py
+++ src/days/day15/main.py
@@ -4,35 +4,51 @@ import re
 
 def part1():
 	with open("input.txt") as input_file:
-		parts = input_file.read().strip().replace('/n', '').split(',')
+		parts = input_file.read().strip().split(',')
 		return sum([eight_bit_hash(p) for p in parts])
 
 
 def part2():
 	with (open("input.txt") as input_file):
-		parts = input_file.read().strip().replace('/n', '').split(',')
+		parts = input_file.read().strip().split(',')
 		hashmap = defaultdict(list)
 		labelmap = defaultdict(list)
 		for p in parts:
-			label, *lens = re.split('[-=]', p)
-			hash_label = eight_bit_hash(label)
+			label, _ = re.split('[-=]', p)
 			if "-" in p:
 				for box in labelmap[label]:
-					hashmap[box] = [v for v in hashmap[box] if not v.startswith(p[:-1])]
+					label_pattern = p.replace("-", "=")
+					tmp = [v for v in hashmap[box] if not v.startswith(label_pattern)]
+					if not tmp == hashmap[box]:
+						print("dropped " + p)
+						print(tmp)
+						print(hashmap[box])
+						print(" ")
+					hashmap[box] = tmp
 				labelmap[label] = []
 			else:
+				if label == "tqjk":
+					print("what")
+				hash_label = eight_bit_hash(label)
 				if not hashmap[hash_label]:
 					hashmap[hash_label].append(p)
 					labelmap[label].append(hash_label)
+					continue
 				else:
-					tmp = [p if e.startswith(label) else e for e in hashmap[hash_label]]
-					if tmp == hashmap[hash_label]:
+					tmp = [p if e.startswith(label + "=") else e for e in hashmap[hash_label]]
+					if hashmap[hash_label] == tmp:
 						hashmap[hash_label].append(p)
+						hashmap[hash_label] = list(dict.fromkeys(hashmap[hash_label]))
 						labelmap[label].append(hash_label)
+						labelmap[label] = list(set(labelmap[label]))
 					else:
 						hashmap[hash_label] = tmp
 		print(hashmap)
 		return get_focusing_power(hashmap)
+
+
+def matches_label(target, v):
+	return target == v.split("=")[0]
 
 
 def get_focusing_power(hashmap):
@@ -57,3 +73,4 @@ if __name__ == "__main__":
 	print(part1())
 	print(part2())
 	#330336 HIGH
+	#329680 HIGH
